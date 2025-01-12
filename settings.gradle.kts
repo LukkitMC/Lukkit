1a2a2fc67a104afe2cce85650b2e28387b020cd9
LukkitMC <LukkitMC@outlook.com>
2025-01-12 17:01:10 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+import java.util.Locale
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = "error:".trimIndent()
+    error(errorText)
+}
+
+rootProject.name = "lukkit"
+
+for (name in listOf("lukkit-api", "lukkit-server")) {
+    val projName = name.lowercase(Locale.ENGLISH)
+    include(projName)
+    file(name).mkdirs()
+    findProject(":$projName")!!.projectDir = file(name)
+}
+
+fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
+    val settingsFile = file("$name.settings.gradle.kts")
+    if (settingsFile.exists()) {
+        apply(from = settingsFile)
+        findProject(":$name")?.let { op?.invoke(it) }
+    } else {
+        settingsFile.writeText(
+            """
+            // Uncomment to enable the '$name' project
+            // include(":$name")
+
+            """.trimIndent()
+        )
+    }
+}
